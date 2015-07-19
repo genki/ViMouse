4aa6e9a018429b4fc4ecc0d141053e2f03b64b28
瀧内元気 <genki@s21g.com>
2015-07-19 12:58:06 +0900

    Initial Commit

---
+++ ViMouse/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ViMouse
+//
+//  Created by TakiuchiGenki on 2015/07/19.
+//  Copyright © 2015年 s21g Inc. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
---
+++ ViMouseUITests/ViMouseUITests.swift
@@ -0,0 +1,34 @@
+//
+//  ViMouseUITests.swift
+//  ViMouseUITests
+//
+//  Created by TakiuchiGenki on 2015/07/19.
+//  Copyright © 2015年 s21g Inc. All rights reserved.
+//
+
+import XCTest
+
+class ViMouseUITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+}
