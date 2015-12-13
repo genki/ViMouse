43d1fd86a48105b3b1baf931d57536110107497e
TakiuchiGenki <genki@s21g.com>
2015-12-13 19:34:25 +0900

    Added quit menu

--- ViMouse/AppDelegate.swift
+++ ViMouse/AppDelegate.swift
@@ -19,6 +19,7 @@ class AppDelegate: NSObject, NSApplicationDelegate, InputHookDelegate {
     var _leftButton = false
     var _rightButton = false
     var _centerButton = false
+    var _baseSpeed:CGFloat = 1.33
     var _dx:CGFloat = 0.0, _dy:CGFloat = 0.0
     var _vx:CGFloat = 0.0, _vy:CGFloat = 0.0
     let _ax:CGFloat = 2.0, _ay:CGFloat = -2.0
@@ -49,11 +50,28 @@ class AppDelegate: NSObject, NSApplicationDelegate, InputHookDelegate {
         icon.template = true
         icon.resizingMode = .Stretch
         icon.size = NSSize(width: 16, height: 16)
-        
-        //_statusItem.title = "ViMouse"
         _statusItem.image = icon
-        //_statusItem.target = self
-        //_statusItem.action = Selector("open:")
+        
+        let menu = NSMenu()
+        _statusItem.menu = menu
+        
+        /*let pref = NSMenuItem()
+        pref.title = "Preferences"
+        pref.action = Selector("config:")
+        menu.addItem(pref)*/
+
+        let quit = NSMenuItem()
+        quit.title = "Quit"
+        quit.action = Selector("quit:")
+        menu.addItem(quit)
+    }
+    /*
+    @IBAction func config(sender: NSButton){
+        NSApp.mainWindow?.makeKeyAndOrderFront(self)
+    }*/
+    
+    @IBAction func quit(sender: NSButton) {
+        NSApp.terminate(self)
     }
     
     func applicationDidFinishLaunching(aNotification: NSNotification) {
@@ -66,150 +84,16 @@ class AppDelegate: NSObject, NSApplicationDelegate, InputHookDelegate {
         // Insert code here to tear down your application
         _inputHook.disable()
     }
-
-    // MARK: - Core Data stack
-
-    lazy var applicationDocumentsDirectory: NSURL = {
-        // The directory the application uses to store the Core Data store file. This code uses a directory named "com.s21g.ViMouse" in the user's Application Support directory.
-        let urls = NSFileManager.defaultManager().URLsForDirectory(.ApplicationSupportDirectory, inDomains: .UserDomainMask)
-        let appSupportURL = urls[urls.count - 1]
-        return appSupportURL.URLByAppendingPathComponent("com.s21g.ViMouse")
-    }()
-
-    lazy var managedObjectModel: NSManagedObjectModel = {
-        // The managed object model for the application. This property is not optional. It is a fatal error for the application not to be able to find and load its model.
-        let modelURL = NSBundle.mainBundle().URLForResource("ViMouse", withExtension: "momd")!
-        return NSManagedObjectModel(contentsOfURL: modelURL)!
-    }()
-
-    lazy var persistentStoreCoordinator: NSPersistentStoreCoordinator = {
-        // The persistent store coordinator for the application. This implementation creates and returns a coordinator, having added the store for the application to it. (The directory for the store is created, if necessary.) This property is optional since there are legitimate error conditions that could cause the creation of the store to fail.
-        let fileManager = NSFileManager.defaultManager()
-        var failError: NSError? = nil
-        var shouldFail = false
-        var failureReason = "There was an error creating or loading the application's saved data."
-
-        // Make sure the application files directory is there
-        do {
-            let properties = try self.applicationDocumentsDirectory.resourceValuesForKeys([NSURLIsDirectoryKey])
-            if !properties[NSURLIsDirectoryKey]!.boolValue {
-                failureReason = "Expected a folder to store application data, found a file \(self.applicationDocumentsDirectory.path)."
-                shouldFail = true
-            }
-        } catch  {
-            let nserror = error as NSError
-            if nserror.code == NSFileReadNoSuchFileError {
-                do {
-                    try fileManager.createDirectoryAtPath(self.applicationDocumentsDirectory.path!, withIntermediateDirectories: true, attributes: nil)
-                } catch {
-                    failError = nserror
-                }
-            } else {
-                failError = nserror
-            }
-        }
-        
-        // Create the coordinator and store
-        var coordinator: NSPersistentStoreCoordinator? = nil
-        if failError == nil {
-            coordinator = NSPersistentStoreCoordinator(managedObjectModel: self.managedObjectModel)
-            let url = self.applicationDocumentsDirectory.URLByAppendingPathComponent("CocoaAppCD.storedata")
-            do {
-                try coordinator!.addPersistentStoreWithType(NSXMLStoreType, configuration: nil, URL: url, options: nil)
-            } catch {
-                failError = error as NSError
-            }
-        }
-        
-        if shouldFail || (failError != nil) {
-            // Report any error we got.
-            var dict = [String: AnyObject]()
-            dict[NSLocalizedDescriptionKey] = "Failed to initialize the application's saved data"
-            dict[NSLocalizedFailureReasonErrorKey] = failureReason
-            if failError != nil {
-                dict[NSUnderlyingErrorKey] = failError
-            }
-            let error = NSError(domain: "YOUR_ERROR_DOMAIN", code: 9999, userInfo: dict)
-            NSApplication.sharedApplication().presentError(error)
-            abort()
-        } else {
-            return coordinator!
-        }
-    }()
-
-    lazy var managedObjectContext: NSManagedObjectContext = {
-        // Returns the managed object context for the application (which is already bound to the persistent store coordinator for the application.) This property is optional since there are legitimate error conditions that could cause the creation of the context to fail.
-        let coordinator = self.persistentStoreCoordinator
-        var managedObjectContext = NSManagedObjectContext(concurrencyType: .MainQueueConcurrencyType)
-        managedObjectContext.persistentStoreCoordinator = coordinator
-        return managedObjectContext
-    }()
-
-    // MARK: - Core Data Saving and Undo support
-
-    @IBAction func saveAction(sender: AnyObject!) {
-        // Performs the save action for the application, which is to send the save: message to the application's managed object context. Any encountered errors are presented to the user.
-        if !managedObjectContext.commitEditing() {
-            NSLog("\(NSStringFromClass(self.dynamicType)) unable to commit editing before saving")
-        }
-        if managedObjectContext.hasChanges {
-            do {try managedObjectContext.save()}
-            catch {
-                let nserror = error as NSError
-                NSApplication.sharedApplication().presentError(nserror)
-            }
-        }
-    }
-
-    func windowWillReturnUndoManager(window: NSWindow) -> NSUndoManager? {
-        // Returns the NSUndoManager for the application. In this case, the manager returned is that of the managed object context for the application.
-        return managedObjectContext.undoManager
-    }
-
-    func applicationShouldTerminate(sender: NSApplication) -> NSApplicationTerminateReply {
-        // Save changes in the application's managed object context before the application terminates.
-        
-        if !managedObjectContext.commitEditing() {
-            NSLog("\(NSStringFromClass(self.dynamicType)) unable to commit editing to terminate")
-            return .TerminateCancel
-        }
-        if !managedObjectContext.hasChanges {return .TerminateNow}
-        
-        do {try managedObjectContext.save()}
-        catch {
-            let nserror = error as NSError
-            // Customize this code block to include application-specific recovery steps.
-            let result = sender.presentError(nserror)
-            if (result) {return .TerminateCancel}
-            
-            let question = NSLocalizedString("Could not save changes while quitting. Quit anyway?", comment: "Quit without saves error question message")
-            let info = NSLocalizedString("Quitting now will lose any changes you have made since the last successful save", comment: "Quit without saves error question info");
-            let quitButton = NSLocalizedString("Quit anyway", comment: "Quit anyway button title")
-            let cancelButton = NSLocalizedString("Cancel", comment: "Cancel button title")
-            let alert = NSAlert()
-            alert.messageText = question
-            alert.informativeText = info
-            alert.addButtonWithTitle(quitButton)
-            alert.addButtonWithTitle(cancelButton)
-            
-            let answer = alert.runModal()
-            if answer == NSAlertFirstButtonReturn {
-                return .TerminateCancel
-            }
-        }
-        // If we got here, it is time to quit.
-        return .TerminateNow
-    }
-
-    /*func applicationDidBecomeActive(notification: NSNotification) {
+    /*
+    func applicationDidBecomeActive(notification: NSNotification) {
         _inputHook.disable()
         NSApp.activateIgnoringOtherApps(true)
         NSApp.mainWindow?.makeKeyAndOrderFront(self)
-    }
+    }*/
     
     func applicationDidResignActive(notification: NSNotification) {
         _inputHook.enable()
-    }*/
+    }
     func tick(){
         var displayID = CGMainDisplayID()
         var rect = CGDisplayBounds(displayID)
@@ -234,7 +118,7 @@ class AppDelegate: NSObject, NSApplicationDelegate, InputHookDelegate {
         _vx *= 0.8
         _vy *= 0.8
         
-        var vx = _vx, vy = _vy
+        var vx = _vx*_baseSpeed, vy = _vy*_baseSpeed
         if(_speedFaster){ vx *= 4; vy *= 4 }
         if(_speedFast){ vx *= 2; vy *= 2 }
         if(_speedSlow){ vx /= 2; vy /= 2 }
